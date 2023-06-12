82bcc25e883e2b13a37b768f60237f84b2354720
yamamoto <arlongtwentylemons@gmail.com>
2023-06-12 11:34:49 +0400

    minor changes

--- packages/wheels_package/src/wheels_node.py
+++ packages/wheels_package/src/wheels_node.py
@@ -4,20 +4,17 @@ import os
 import rospy
 from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import WheelsCmdStamped, Twist2DStamped, BoolStamped
+from std_msgs.msg import String
 
 
 HOST = os.environ['VEHICLE_NAME']
 cmd = f'/{HOST}/car_cmd_switch_node/cmd'
-car_cmd = f'/{HOST}/joy_mapper_node/car_cmd'
-velocity = f'/{HOST}/kinematics_node/velocity'
-wheels_cmd = f'/{HOST}/wheels_driver_node/wheels_cmd'
 wheels_cmd_executed = f'/{HOST}/wheels_driver_node/wheels_cmd_executed'
 
 class WheelsNode(DTROS):
     def __init__(self, node_name):
         super(WheelsNode, self).__init__(node_name=node_name, node_type=NodeType.CONTROL)
         # testing for all Twist2DStamped, WheelsCmdStamped
-
 
         # CMD
         self.cmd_pub = rospy.Publisher(
@@ -33,92 +30,66 @@ class WheelsNode(DTROS):
             queue_size=1
         )
 
-        # CMD_PUB
-        self.car_cmd_pub = rospy.Publisher(
-            car_cmd,
-            Twist2DStamped,
-            queue_size=1
-        )
-
-        self.car_cmd_sub = rospy.Subscriber(
-            car_cmd,
-            Twist2DStamped,
-            self.car_cmd_cb,
-            queue_size=1
-        )
-
-        # VELOCITY
-        self.velocity_pub = rospy.Publisher(
-            velocity,
-            Twist2DStamped,
-            queue_size=1
-        )
-
-        self.velocity_sub = rospy.Subscriber(
-            velocity,
-            Twist2DStamped,
-            self.velocity_cb,
-            queue_size=1
-        )
-
-        # WHEELS_CMD
-        self.wheels_cmd_pub = rospy.Publisher(
-            wheels_cmd,
-            WheelsCmdStamped,
-            queue_size=1
-        )
-
-        self.wheels_cmd_sub = rospy.Subscriber(
-            wheels_cmd,
-            WheelsCmdStamped,
-            self.wheels_cmd_cb,
-            queue_size=1
-        )
-
         # WHEELS_CMD_EXECUTED
-        self.wheels_cmd_executed_pub = rospy.Publisher(
+        self.wheels_pub = rospy.Publisher(
             wheels_cmd_executed,
             WheelsCmdStamped,
             queue_size=1
         )
 
-        self.wheels_cmd_executed_sub = rospy.Subscriber(
+        self.wheels_sub = rospy.Subscriber(
             wheels_cmd_executed,
             WheelsCmdStamped,
-            self.wheels_cmd_executed_cb,
+            self.wheels_cb,
             queue_size=1
         )
 
     def cmd_cb(self, twist):
-        rospy.loginfo(f'cmd received {type(twist)}')
+        rospy.loginfo(f'Twist: v={twist.v}, omega={twist.omega}')
 
-    def car_cmd_cb(self, twist):
-        rospy.loginfo(f'cmd received {type(twist)}')
+    def wheels_cb(self, wheel):
+        rospy.loginfo(f'Wheel: left={wheel.vel_left}, right={wheel.vel_right}')
 
-    def velocity_cb(self, twist):
-        rospy.loginfo(f'cmd received {type(twist)}')
-        
+    def stop(self):
+        wh = self.get_wheel(0,0)
+        tw = self.get_twist(0,0)
+        self.move(wheel=wh, twist=tw)
 
-    def wheels_cmd_cb(self, wheels):
-        rospy.loginfo(f'cmd received {type(wheels)}')
-
-    def wheels_cmd_executed_cb(self, wheels):
-        rospy.loginfo(f'cmd received {type(wheels)}')
 
     def run(self):
+
+        rospy.Rate(0.5).sleep()
+        wh = self.get_wheel(0.5, 0.5)
+        tw = self.get_twist(20, 0)
+
+        self.move(wheel=wh, twist=tw)
+
+        rospy.Rate(0.5).sleep()
+        rospy.loginfo('-------------------')
+
+        wh = self.get_wheel(1, 0.2)
+        tw = self.get_twist(10, 10)
+        self.move(wh, tw)
+
+        rospy.Rate(1).sleep()
+
+        self.stop()
+
+    def get_wheel(self, l, r):
         wheel = WheelsCmdStamped()
-        wheel.vel_right = 1
-        wheel.vel_left = 1
+        wheel.vel_left = l
+        wheel.vel_right = r
+        return wheel
 
+    def get_twist(self, v, omega):
         twist = Twist2DStamped()
-        twist.v = 40
-        twist.omega = 10
+        twist.v = v
+        twist.omega = omega
+        return twist
 
-        while not rospy.is_shutdown():
-            self.wheels_cmd_pub.publish(wheel)
-            self.wheels_cmd_executed_pub.publish(wheel)
-            self.cmd_pub.publish(twist)
-
+    def move(self, wheel, twist):
+        self.wheels_pub.publish(wheel)
+        self.cmd_pub.publish(twist)
 
 if __name__ == '__main__':
     print('started')
